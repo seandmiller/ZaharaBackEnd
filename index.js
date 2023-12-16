3906725f7b54de5f8e68a69a424e32bd766d7ec1
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2023-12-15 19:01:55 -0800

    updating cors

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 
 var corsOptions = {
     origin: ['http://localhost:3000', 'https://mentalhphelp.com', 'http://127.0.0.1:3000'],
-    optionsSuccessStatus: 200 
+    optionsSuccessStatus: 200 ,
+    credentials:true
     }
 
 
