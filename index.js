5b5decc6c30796303a04973d2e3583a2d1f4d4ea
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2023-12-18 09:20:06 -0800

    updating cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 var corsOptions = {
-    origin: ['http://localhost:3000', 'https://mentalhphelp.com', 'https://quiet-lowlands-62573-2c3c77d42eb8.herokuapp.com/', '/\.herokuapp\.com$/'],
+    origin: ['http://localhost:3000', 'https://mentalhphelp.com', 'https://quiet-lowlands-62573-2c3c77d42eb8.herokuapp.com/', '/\.herokuapp\.com$/', 'http://fixie:6wCtrZLFRLzZPUc@velodrome.usefixie.com:80'],
     methods:"GET,PUT,POST,DELETE,PATCH",
     credentials:true
 
