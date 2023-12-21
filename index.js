9e45daf9042f389c72bfca991ca70217d5b183df
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2023-12-20 18:05:33 -0800

    updating cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var corsOptions = {
 
     }
 
-app.options('*', cors());
+
 app.use(cors(corsOptions));
 
 
@@ -35,7 +35,7 @@ app.use(cors(corsOptions));
 // });
 app.use(express.json());
 app.use('/api', routes);
-
+app.options('*', cors());
 const PORTLOCAL = 8080;
 
 const mongoString = process.env.DATABASE_URL;
